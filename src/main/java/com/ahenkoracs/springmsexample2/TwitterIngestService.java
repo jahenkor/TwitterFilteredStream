3673bb0369bffd9765fd6f81e9543a1de98c3bb9
Julius Ahenkora <juliusahenkora@gmail.com>
2024-09-20 10:45:04 -0600

    reformatting class names

--- src/main/java/com/ahenkoracs/springmsexample2/TwitterIngestService.java
+++ src/main/java/com/ahenkoracs/springmsexample2/TwitterIngestService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMsExample2Application {
+public class TwitterIngestService {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMsExample2Application.class, args);
+        SpringApplication.run(TwitterIngestService.class, args);
     }
 
 }
